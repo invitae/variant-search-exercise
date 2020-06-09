81a96faa8cb98a9342aff90c2f01925c47ddbef9
Shane Giles <shane.giles@invitae.com>
2020-06-08 22:44:16 -0600

    Dockerize variant search remove commented lines from entry point script

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Collect static files
-# echo "Collect static files"
-# python manage.py collectstatic --noinput
-
 # Apply database migrations
 echo "Apply database migrations"
 python manage.py migrate
